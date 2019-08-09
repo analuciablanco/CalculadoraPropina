f30b69a3f4aa5fad28c663920ce3d65b56ee9f62
Alumno <ulsa@iMacD103.local>
2019-08-09 13:24:35 -0700

    Primer commit

--- CalculadoraPropina/ViewController.swift
+++ CalculadoraPropina/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Variables (outlets)
+    @IBOutlet weak var txtTotalSinPropina: UITextField!
+    
+    @IBOutlet weak var sldPorcentajePropina: UISlider!
+    
+    @IBOutlet weak var lblPorcentajePropina: UILabel!
+    
+    @IBOutlet weak var lblCantidadPropina: UILabel!
+    
+    @IBOutlet weak var lblTotalConPropina: UILabel!
+    
+    
+    // Acciones (actions)
+    @IBAction func doChangeValores(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
